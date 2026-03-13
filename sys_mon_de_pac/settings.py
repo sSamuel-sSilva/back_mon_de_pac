87c57c5ff7f728ffb906d85c70404bec1ef84ae7
DiogoBramorim <diogobamorim06@gmail.com>
2026-03-13 00:33:04 -0300

    ajustando as pastas

--- sys_mon_de_pac/settings.py
+++ sys_mon_de_pac/settings.py
@@ -96,10 +96,11 @@ CHANNEL_LAYERS = {
 
 # DATABASE AUTOMÁTICO
 
+import dj_database_url
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3"
     )
 }
 
